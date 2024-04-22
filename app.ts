b5ff568465afcc08f67e6cd621989f83576bb806
Hanif8193 <hanifdostmuhammad8193@gmail.com>
2024-04-22 11:09:02 +0500

    push my word counter to github

---
+++ app.ts
@@ -0,0 +1,18 @@
+#! usr/bin/env node
+import inquirer from "inquirer"
+
+const answer: {
+    sentence:string
+} = await inquirer.prompt([
+    {
+       name: "sentence",
+       type: "input" ,
+       message: ("enter your sentence to count the words")
+    }
+])
+const words = answer. sentence. trim(). split(" ");
+
+console.log (words);
+
+console.log(`your sentence words is   ${words.length}`);
+
